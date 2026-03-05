a5fa1a43979112d2ebfff38e8fa9ddbaa6f3954a
mspark9 <stellaaa0906@gmail.com>
2026-03-05 11:11:10 +0900

    create forgot-password

--- src/App.jsx
+++ src/App.jsx
@@ -8,39 +8,42 @@ import DailyLogPage from './pages/DailyLogPage';
 import ReportPage from './components/Report/ReportPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import ForgotPasswordPage from './pages/ForgotPasswordPage';
 import ScanAnalysisPage from './components/scanAnalysis/ScanAnalysisPage';
 import RecommendPage from './components/Recommend/RecommendPage';
 
 function App() {
   return (
     <AuthProvider>
-    <BrowserRouter>
-      <Routes>
-        {/* 랜딩 페이지 (첫 진입) */}
-        <Route path="/" element={<LandingPage />} />
+      <BrowserRouter>
+        <Routes>
+          {/* 랜딩 페이지 (첫 진입) */}
+          <Route path="/" element={<LandingPage />} />
 
-        {/* 인증 페이지 (Layout 없이 전체 화면) */}
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
+          {/* 인증 페이지 (Layout 없이 전체 화면) */}
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/forgot-password" element={<ForgotPasswordPage
+          />} />
 
-        {/* 메인 앱 (Layout 포함) */}
-        <Route path="/home" element={<Layout />}>
-          <Route index element={<HomePage />} />
-          <Route path="recommendation" element={<RecommendPage />} />
-          <Route path="scanAnalysis" element={<ScanAnalysisPage />} />
-          <Route path="dailyLog" element={<DailyLogPage />} />
-          <Route path="report" element={<ReportPage />} />
-          <Route
-            path="notifications"
-            element={<PlaceholderPage title="알림" />}
-          />
-          <Route
-            path="settings"
-            element={<PlaceholderPage title="환경설정" />}
-          />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+          {/* 메인 앱 (Layout 포함) */}
+          <Route path="/home" element={<Layout />}>
+            <Route index element={<HomePage />} />
+            <Route path="recommendation" element={<RecommendPage />} />
+            <Route path="scanAnalysis" element={<ScanAnalysisPage />} />
+            <Route path="dailyLog" element={<DailyLogPage />} />
+            <Route path="report" element={<ReportPage />} />
+            <Route
+              path="notifications"
+              element={<PlaceholderPage title="알림" />}
+            />
+            <Route
+              path="settings"
+              element={<PlaceholderPage title="환경설정" />}
+            />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </AuthProvider>
   );
 }
